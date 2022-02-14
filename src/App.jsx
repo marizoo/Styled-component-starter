3e3668438e50652c1d09753fc293771230ae25d8
marizoo <marizoo@meow.woof>
2022-02-14 16:55:18 +0700

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import styled from "styled-components";
-import Button from "./UI/button/Button";
 import Hero from "./components/hero/Hero";
 import ToggleTheme from "./UI/toggleTheme/ToggleTheme";
-// import { RiSunLine, RiMoonLine } from "react-icons/ri";
-// import { Routes, Route, Link } from "react-router-dom";
 
 const ScApp = styled.div`
     width: 100vw;
@@ -36,5 +33,3 @@ const App = () => {
 };
 
 export default App;
-
-// https://www.youtube.com/watch?v=377AQ0y6LPA
